1a9cfcbee5c9592fc1bd2cb31a089262e7c4a774
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-08-04 18:48:51 +0100

    commit : styling

--- components/ExpensesOutput.js/ExpenseItem.js
+++ components/ExpensesOutput.js/ExpenseItem.js
@@ -1,4 +1,5 @@
-import { Pressable, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
 function ExpenseItem({ description, amount, date }) {
   return (
@@ -17,3 +18,28 @@ function ExpenseItem({ description, amount, date }) {
 }
 
 export default ExpenseItem;
+
+const styles = StyleSheet.create({
+  expenseItem: {
+    padding: 12,
+    marginVertical: 8,
+    backgroundColor: GlobalStyles.colors.primary500,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderRadius: 6,
+    elevation: 3,
+    shadowColor: GlobalStyles.colors.gray500,
+    shadowRadius: 4,
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.4,
+  },
+  textBase: {
+    color: GlobalStyles.colors.primary50,
+  },
+  description: {
+    fontSize: 16,
+    marginBottom: 4,
+    fontWeight: "bold",
+  },
+  amountContainer: {},
+});
