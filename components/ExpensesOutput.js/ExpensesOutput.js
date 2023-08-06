18288fb42b381f007300765328f9ac935170de0f
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-08-06 21:18:49 +0100

    ... commit

--- components/ExpensesOutput.js/ExpensesOutput.js
+++ components/ExpensesOutput.js/ExpensesOutput.js
@@ -3,63 +3,6 @@ import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 import { GlobalStyles } from "../../constants/styles";
 
-const DUMMY_Expenses = [
-  {
-    id: "e1",
-    description: "A pair of shoes",
-    amount: 59.99,
-    date: new Date("2021-12-19"),
-  },
-  {
-    id: "e2",
-    description: "A pair of trousers",
-    amount: 9.99,
-    date: new Date("2022-12-19"),
-  },
-  {
-    id: "e3",
-    description: "Some bananas ",
-    amount: 79.99,
-    date: new Date("2023-12-19"),
-  },
-  {
-    id: "e4",
-    description: "Another book ",
-    amount: 18.99,
-    date: new Date("2021-09-20"),
-  },
-  {
-    id: "e5",
-    description: "A pair of shoes",
-    amount: 59.99,
-    date: new Date("2021-12-19"),
-  },
-  {
-    id: "e6",
-    description: "A pair of trousers",
-    amount: 9.99,
-    date: new Date("2022-12-19"),
-  },
-  {
-    id: "e7",
-    description: "Some bananas ",
-    amount: 79.99,
-    date: new Date("2023-12-19"),
-  },
-  {
-    id: "e8",
-    description: "Another book ",
-    amount: 18.99,
-    date: new Date("2021-09-20"),
-  },
-  {
-    id: "e9",
-    description: "Another book ",
-    amount: 18.99,
-    date: new Date("2021-09-20"),
-  },
-];
-
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View style={styles.container}>
--- store/expenses-context.js
+++ store/expenses-context.js
@@ -1,5 +1,62 @@
 import { createContext, useReducer } from "react";
 
+const DUMMY_Expenses = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2021-12-19"),
+  },
+  {
+    id: "e2",
+    description: "A pair of trousers",
+    amount: 9.99,
+    date: new Date("2022-12-19"),
+  },
+  {
+    id: "e3",
+    description: "Some bananas ",
+    amount: 79.99,
+    date: new Date("2023-12-19"),
+  },
+  {
+    id: "e4",
+    description: "Another book ",
+    amount: 18.99,
+    date: new Date("2021-09-20"),
+  },
+  {
+    id: "e5",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2021-12-19"),
+  },
+  {
+    id: "e6",
+    description: "A pair of trousers",
+    amount: 9.99,
+    date: new Date("2022-12-19"),
+  },
+  {
+    id: "e7",
+    description: "Some bananas ",
+    amount: 79.99,
+    date: new Date("2023-12-19"),
+  },
+  {
+    id: "e8",
+    description: "Another book ",
+    amount: 18.99,
+    date: new Date("2021-09-20"),
+  },
+  {
+    id: "e9",
+    description: "Another book ",
+    amount: 18.99,
+    date: new Date("2021-09-20"),
+  },
+];
+
 export const ExpensesContext = cretaeContext({
   expenses: [],
   addExpense: ({ description, amount, date }) => {},
@@ -10,7 +67,17 @@ export const ExpensesContext = cretaeContext({
 function expensesReducer(state, action) {
   switch (action.type) {
     case "ADD":
+      const id = new Date().toString() + Math.random().toString();
+      return [{ ...action.payload, id: id }, ...state];
     case "UPDATE":
+      const updatableExpenseIndex = state.findIndex(
+        (expense) => expense.id === action.payload.id
+      );
+      const updatableExpense = state[updatableExpenseIndex];
+      const updatedItem = { ...updateExpense, ...action.payload.data };
+      const updatedExpenses = [...state];
+      updatedExpenses[updatableExpenseIndex] = updatedItem;
+      return updatedExpenses;
     case "DELETE":
     default:
       return state;
@@ -18,7 +85,7 @@ function expensesReducer(state, action) {
 }
 
 function ExpensesContextProvider({ children }) {
-  const [expenseState, dispatch] = useReducer(expensesReducer);
+  const [expenseState, dispatch] = useReducer(expensesReducer, DUMMY_Expenses);
 
   function addExpense(expenseData) {
     dispatch({ type: "ADD", payload: expenseData });
